c77dd8b6a28b524d8b2eb3bf2d643057e6341996
Nicolás Peña Henríquez <cs.cubes.2001@gmail.com>
2023-03-31 23:32:36 -0300

    feat: add animations when player moves

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -7,8 +7,14 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var pivot = $Pivot
+@onready var animationPlayer = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/playback")
+
 
 func _physics_process(delta):
+	var direction = get_movement()
 	# Add the gravity.
 	if not is_on_floor():
 		apply_gravity(delta)
@@ -19,8 +25,16 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_pressed("attack"):
 		attack()
-
-	get_movement()
+	
+	# handle anmimations
+	handle_sprite_direction(direction)
+	
+	if abs(velocity.x) > 0:
+		playback.travel("run")
+	
+	if not velocity.x:
+		playback.travel("idle")
+	
 	move_and_slide()
 
 func apply_gravity(delta):
@@ -36,6 +50,11 @@ func get_movement():
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	return direction
+	
 func attack():
 	pass
-	
+
+func handle_sprite_direction(direction):
+	if direction:
+		pivot.scale.x = sign(direction)
