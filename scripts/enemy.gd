ce69a6b9ac03ee719829703c571b01efd82a02f0
Nicolás Peña Henríquez <cs.cubes.2001@gmail.com>
2023-04-12 15:15:38 -0400

    player can attack an enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,23 +6,24 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_killable = false
 
 @onready var animationPlayer = $AnimationPlayer
-@onready var animation_tree = $AnimationTree
-@onready var playback = animation_tree.get("parameters/playback")
-
-func _ready():
-	animation_tree.active = true
+#@onready var animation_tree = $AnimationTree
+#@onready var playback = animation_tree.get("parameters/playback")
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	handle_movement_animations()
+		
 
 	move_and_slide()
 
-func handle_movement_animations():
-	if not velocity.x:
-		playback.travel("idle")
+func set_is_killable(argument: bool):
+	is_killable = argument
+
+func _on_area_2d_area_entered(area):
+	$AnimationPlayer.play("idle_turn")
+	if is_killable:
+		queue_free()
