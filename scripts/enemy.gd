b4c70492eb4a9865f0376589cba79ed765e81af4
Felipe Jara <fjararibet@gmail.com>
2023-04-12 08:10:38 -0400

    handle enemy idle animation

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -7,22 +7,22 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animationPlayer = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/playback")
+
+func _ready():
+	animation_tree.active = true
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	handle_movement_animations()
 
 	move_and_slide()
+
+func handle_movement_animations():
+	if not velocity.x:
+		playback.travel("idle")
