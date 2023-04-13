0736be5fed1e4e4cdd36ef8d66394ae4f01dc187
Nicolás Peña Henríquez <cs.cubes.2001@gmail.com>
2023-04-13 14:50:23 -0400

    added bullet spawning and player hurtbox

---
+++ scripts/bullet_test.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var SPEED = 20
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animated_sprite_2d.play("default")
+
+func _physics_process(delta):
+	global_position.x -= SPEED * delta
+	
+
+
+func _on_area_2d_area_entered(area):
+	queue_free()
---
+++ scripts/world.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+
+
+func _input(event):
+	if event.is_action_pressed("spawn_bullet"):
+		var bullet_resource = preload("res://scenes/bullet_test.tscn")
+		var bullet = bullet_resource.instantiate()
+		bullet.position = Vector2(865, 18)
+		add_child(bullet)
