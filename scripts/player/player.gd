869307c9fe997a7081897a02833bce53e6920189
Nicolás I. Peña Henríquez <cs.cubes.2001@gmail.com>
2023-05-17 19:59:55 -0400

    jump height improved

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -46,12 +46,13 @@ func _physics_process(delta):
 
 	if not is_on_floor():
 		apply_gravity(delta)
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		jump()
 		
-	if Input.is_action_just_released("jump") and velocity.y <= 0:
-		velocity.y /= 5
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			jump()
+	else:
+		if Input.is_action_just_released("jump") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
 		
 	if Input.is_action_just_pressed("attack"):
 		attack()
