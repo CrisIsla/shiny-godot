39b29a3140ad6ee95f12e32aeb451a440a5d72a7
fjararibet <fjararibet@gmail.com>
2023-05-20 22:14:20 -0400

    add turn function to enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 class_name Enemy
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var time = 0
+
+
 
 @export var speed: int
 @export var jump_velocity: int
@@ -13,7 +16,30 @@ func _physics_process(delta):
 	if not is_on_floor():
 		apply_gravity(delta)
 	
+	time += delta
+	if time > 5:
+		_on_hit_turn()
+		time = 0
+	
 	move_and_slide()
 
 func apply_gravity(delta):
 	velocity.y += gravity * delta
+	
+
+func _on_hit_turn():
+	var pivot = get_node("TurnPivot/Pivot")
+	var turn_pivot = get_node("TurnPivot")
+	turn_pivot.skew = 0
+	
+	# rotate
+	var tween = get_tree().create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	
+	tween.tween_property(turn_pivot, "skew", 6 * PI, 3)
+
+	
+	
+	
+	
+	
