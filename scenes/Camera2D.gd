c1d03910eba0b0215e5ed53eb05d2940f2435ee1
fjararibet <fjararibet@gmail.com>
2023-05-21 16:35:11 -0400

    fix how tweens are used

--- scenes/Camera2D.gd
+++ scenes/Camera2D.gd
@@ -1,22 +1,26 @@
 extends Camera2D
 
 var facing = 0
-const LOOK_AHEAD_FACTOR = 0.07
+const LOOK_AHEAD_FACTOR = 0.05
 
 const SHIFT_TRANS = Tween.TRANS_SINE
 const SHIFT_EASE = Tween.EASE_OUT
 const SHIFT_DURATION = 1.0
 
 @onready var prev_cam_pos = get_target_position()
-@onready var tween = create_tween()
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	_check_facing()
 	prev_cam_pos = get_target_position()
 	
@@ -24,9 +28,12 @@ func _process(delta):
 func _check_facing():
 	var new_facing  = sign(get_target_position().x - prev_cam_pos.x)
 	if new_facing != 0 && facing != new_facing:
+		var tween = get_tree().create_tween()
+		tween.set_trans(SHIFT_TRANS).set_ease(SHIFT_EASE)
 		facing = new_facing
 		var target_offset = get_viewport_rect().size.x * LOOK_AHEAD_FACTOR * facing
-		tween.interpolate_value(position.x, target_offset - position.x, 0, SHIFT_DURATION, SHIFT_TRANS, SHIFT_EASE)
+		tween.tween_property(self, "position", Vector2(target_offset, position.y), SHIFT_DURATION)
+
 		
 	
 func _on_player_grounded_updated(is_grounded):
