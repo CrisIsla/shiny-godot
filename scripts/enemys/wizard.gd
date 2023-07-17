c19adfe46d8313db98509d67c0f089f55860f895
Your Name <fjararibet@gmail.com>
2023-07-17 13:31:33 -0400

    Make projectile behave properly

--- scripts/enemys/wizard.gd
+++ scripts/enemys/wizard.gd
@@ -6,11 +6,15 @@ const JUMP_VELOCITY = -400.0
 
 @export var projectile_scene: PackedScene
 
+enum HOR_DIRECTIONS { LEFT = -1, RIGHT = 1 }
+@export var direction: HOR_DIRECTIONS
+
 @onready var animation_player = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/playback")
 @onready var timer = $Timer
 @onready var projectile_spawn = $Pivot/ProjectileSpawn
+@onready var pivot = $Pivot
 
 enum {
 	IDLE, SHOOTING
@@ -20,6 +24,8 @@ var state = IDLE
 func _ready():
 	timer.timeout.connect(_shoot)
 	animation_tree.active = true
+
+	scale.x = direction
 	
 func _physics_process(delta):
 	# Add the gravity.
@@ -43,10 +49,11 @@ func _shoot():
 	state = SHOOTING
 	
 func _spawn_projectile():
+	
 	# Add projectile as a child.
 	var projectile = projectile_scene.instantiate()
-	projectile.initialize(projectile_spawn.position)
-	add_child(projectile)
+	projectile.initialize(direction)
+	projectile_spawn.add_child(projectile)
 	state = IDLE
 
 	
--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -2,17 +2,22 @@ extends Node2D
 
 @export var SPEED = 50
 @onready var animated_sprite_2d = $AnimatedSprite2D
+var direction = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animated_sprite_2d.play("default")
+	scale.x = direction
 
 func _physics_process(delta):
-	global_position.x += SPEED * delta
+	global_position.x += SPEED * delta * direction
+	scale.x = direction
+	
 	
 
-func initialize(initial_position: Vector2):
-	global_position = initial_position
+func initialize(initial_direction: int):
+	direction = initial_direction
+
 	
 func _on_area_2d_area_entered(area):
 #	queue_free()
